32ccd1d843f74fcebe1d4097a7a6eb06294e7fd4
4bjith <abhijithpops1890@gmail.com>
2025-10-07 20:39:29 +0530

    router error of user home corrected

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import RideBooking from '../Pages/RideBooking'
-import Home from '../Pages/Home'
-import UserHome from '../Pages/UserHome'
-import AccountManager from '../Pages/AccountManager'
-import NameEditForm from '../Components/NameEditForm'
-import LoginOrRegister from '../Pages/LoginOrRegister'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RideBooking from "../Pages/RideBooking";
+import Home from "../Pages/Home";
+import UserHome from "../Pages/UserHome";
+import AccountManager from "../Pages/AccountManager";
+import NameEditForm from "../Components/NameEditForm";
+import LoginOrRegister from "../Pages/LoginOrRegister";
 
 export default function CustomeRoute() {
   return (
     <>
-        <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/home-user' element={<UserHome/>}/>
-            <Route path='/bookride' element={<RideBooking/>}/>
-            <Route path='/login' element={<LoginOrRegister/>}/>
-            <Route path='/account' element={<AccountManager/>}/>
-            <Route path='/account/updatename' element={<NameEditForm/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<UserHome/>} />
+          <Route path="/bookride" element={<RideBooking />} />
+          <Route path="/login" element={<LoginOrRegister />} />
+          <Route path="/account" element={<AccountManager />} />
+          <Route path="/account/updatename" element={<NameEditForm />} />
         </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
     </>
-  )
+  );
 }
